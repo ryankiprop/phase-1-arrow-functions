48f9ba83bc4b3456568ded7ccfea092e3ffc308f
Ryan <ryan.kiprop@student.moringaschool.com>
2025-03-14 15:50:43 +0300

    complete

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function(a, b) {
+    return a / b;
+  };
+  
+  const square = (x) => x * x;
+  
+  const add = (a, b) => a + b;
+  
+  module.exports = { divide, square, add };
+  
